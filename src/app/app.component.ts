abe48a8eb7929114670ab83d997cac5ad05411c1
Allan Duong <alland12@hotmail.com>
2024-05-10 00:07:54 +0200

    Add Dashboard and Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatSlideToggle} from '@angular/material/slide-toggle';
+import {NavigationComponent} from './components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, MatSlideToggle],
+    imports: [RouterOutlet, MatSlideToggle, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
