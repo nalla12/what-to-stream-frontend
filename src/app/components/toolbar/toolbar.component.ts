d697d0d8a5df036f236ed398703df04e9f6464a6
Allan Duong <alland12@hotmail.com>
2024-05-13 14:20:27 +0200

    Add top toolbar, link home and svg icon, remove unused nav-components

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
-import {MatIcon} from '@angular/material/icon';
+import {Component} from '@angular/core';
 import {MatIconButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-toolbar',
-  standalone: true,
+    selector: 'app-toolbar',
+    standalone: true,
     imports: [
-        MatIcon,
+        MatIconModule,
         MatIconButton,
         MatToolbar
     ],
-  templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+    templateUrl: './toolbar.component.html',
+    styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
-
+    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+        // Note that we provide the icon here as a string literal here due to a limitation in
+        // Stackblitz. If you want to provide the icon from a URL, you can use:
+        // `iconRegistry.addSvgIcon('thumbs-up', sanitizer.bypassSecurityTrustResourceUrl('icon.svg'));`
+        iconRegistry.addSvgIcon('tv', sanitizer.bypassSecurityTrustResourceUrl('assets/tv.svg'));
+    }
 }
