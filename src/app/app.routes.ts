e75fdbada1e14c2c40b13346b27e61d85710d36d
Allan Duong <alland12@hotmail.com>
2024-05-13 13:53:12 +0200

    Add route for results page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from './components/home-page/home-page.component';
 import {ShowsComponent} from './components/shows/shows.component';
+import {ResultsPageComponent} from './components/results-page/results-page.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomePageComponent,
+        title: 'What To Stream'
+    },
+    {
+        path: 'results',
+        component: ResultsPageComponent,
         title: 'What To Stream'
     },
     {
